fe36e701d0893b61995d76e47d862cea07e16e95
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-06 11:43:38 +0200

    Add and configured the webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import van from "./num";
+import "./style.css";
+
+console.log(van(2,3));
+console.log('hello')
---
+++ src/num.js
@@ -0,0 +1,5 @@
+let van = function num(a, b) {
+    return a + b;
+}
+
+export default van;
